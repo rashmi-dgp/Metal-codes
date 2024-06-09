57b4bbdef5b9fb04a631a667b7eb5282ab617e2a
Rashmi Gupta <119884343+rashmi-dgp@users.noreply.github.com>
2024-06-10 02:20:53 +0530

    Add files via upload

---
+++ Metal-Tutorial/mtlm.h
@@ -0,0 +1,12 @@
+
+
+#pragma once
+#include <simd/simd.h>
+
+namespace mtlm {
+
+    simd::float4x4 identity();
+    simd::float4x4 translation(simd::float3 dPos);
+    simd::float4x4 z_rotation(float theta);
+    simd::float4x4 scale(float factor);
+}
